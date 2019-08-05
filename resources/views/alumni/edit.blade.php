92663ca279761dceac2fb87e13a4004170b4404f
Shadrack <shadrackkimutai@gmail.com>
2019-08-05 04:43:06 +0300

    More Improvement
    Improved to the point of Displaying content but the update function is glitchy

--- resources/views/alumni/edit.blade.php
+++ resources/views/alumni/edit.blade.php
@@ -7,64 +7,84 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Edit Profile') }}</div>
+    @if ($errors->any())
+
+        <div class="alert alert-danger">
+
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+
+            <ul>
+
+                @foreach ($errors->all() as $error)
+
+                    <li>{{ $error }}</li>
+
+                @endforeach
+
+            </ul>
+
+        </div>
+
+    @endif
 
                 <div class="card-body">
-                   <form class="form-horizontal">
+
+                   <form class="form-horizontal" action="{{ route('alumnis.update',$alumni->id) }}" method="POST">
 
 
   <div class="form-group">
     <label for="fullname" class="col-sm-2 control-label">Name</label>
     <div class="col-sm-10">
-      <input class="form-control" id="fullname" disabled>
+      <input class="form-control" id="fullname" value="{{ $alumni->fullname }}" disabled>
     </div>
   </div>
   
   <div class="form-group">
     <label for="idnum" class="col-sm-2 control-label">ID Number</label>
     <div class="col-sm-10">
-      <input class="form-control" id="idnum" disabled>
+      <input class="form-control" id="idnum"  value="{{ $alumni->idnum }}" disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="adm" class="col-sm-2 control-label">Admission Number</label>
     <div class="col-sm-10">
-      <input class="form-control" id="adm" disabled>
+      <input class="form-control" id="adm" value="{{ $alumni->adm }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="mobile" class="col-sm-2 control-label">Mobile Phone Number</label>
     <div class="col-sm-10">
-      <input class="form-control" id="mobile" disabled>
+      <input class="form-control" id="mobile" value="{{ $alumni->mobile }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="email" class="col-sm-2 control-label">Email</label>
     <div class="col-sm-10">
-      <input class="form-control" id="email" type="email" disabled>
+      <input class="form-control" id="email" type="email" value="{{ $alumni->email }}" disabled disabled>
     </div>
   </div>
 
 <div class="form-group">
     <label for="dept" class="col-sm-2 control-label">Department</label>
     <div class="col-sm-10">
-      <input class="form-control" id="dept" disabled>
+      <input class="form-control" id="dept" value="{{ $alumni->dept }}" disabled disabled>
     </div>
   </div>
 
 <div class="form-group">
     <label for="course" class="col-sm-2 control-label">Course</label>
     <div class="col-sm-10">
-      <input class="form-control" id="course" disabled>
+      <input class="form-control" id="course" value="{{ $alumni->course }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="level" class="col-sm-2 control-label">Level</label>
     <div class="col-sm-10">
-      <input class="form-control" id="level" disabled>
+      <input class="form-control" id="level" value="{{ $alumni-> level}}" disabled disabled>
     </div>
   </div>
 
@@ -72,21 +92,21 @@
 <div class="form-group">
     <label for="current_address" class="col-sm-2 control-label">Current Address</label>
     <div class="col-sm-10">
-      <input class="form-control" id="current_address" disabled>
+      <input class="form-control" id="current_address" value="{{ $alumni->current_address }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="permanent_address" class="col-sm-2 control-label">Permanent Address</label>
     <div class="col-sm-10">
-      <input class="form-control" id="permanent_address" disabled>
+      <input class="form-control" id="permanent_address" value="{{ $alumni-> permanent_address}}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="nextofkin" class="col-sm-2 control-label">Next of Kin</label>
     <div class="col-sm-10">
-      <input class="form-control" id="nextofkin" disabled>
+      <input class="form-control" id="nextofkin" value="{{ $alumni->nextofkin }}" disabled disabled>
     </div>
   </div>
 
@@ -94,42 +114,42 @@
   <div class="form-group">
     <label for="nextofkinphone" class="col-sm-2 control-label">Next of Kin Phone</label>
     <div class="col-sm-10">
-      <input class="form-control" id="nextofkinphone" disabled>
+      <input class="form-control" id="nextofkinphone" value="{{ $alumni->nextofkinphone }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="nextofkinadd" class="col-sm-2 control-label">Next of kin Address</label>
     <div class="col-sm-10">
-      <input class="form-control" id="nextofkinadd" disabled>
+      <input class="form-control" id="nextofkinadd" value="{{ $alumni->nextofkinadd }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="occupation" class="col-sm-2 control-label">Occupation</label>
     <div class="col-sm-10">
-      <input class="form-control" id="occupation" disabled>
+      <input class="form-control" id="occupation" value="{{ $alumni->occupation }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="occupation_place" class="col-sm-2 control-label">Occupation Address</label>
     <div class="col-sm-10">
-      <input class="form-control" id="occupation_place" disabled>
+      <input class="form-control" id="occupation_place" value="{{ $alumni->occupation_place }}" disabled disabled>
     </div>
   </div>
 
   <div class="form-group">
     <label for="placeofworkadd" class="col-sm-2 control-label">Occupation Place</label>
     <div class="col-sm-10">
-      <input class="form-control" id="placeofworkadd" disabled>
+      <input class="form-control" id="placeofworkadd" value="{{ $alumni->placeofworkadd }}" disabled disabled>
     </div>
   </div>
 
     <div class="form-group">
     <label for="supervisoradd" class="col-sm-2 control-label">Occupation Supervisor Phone</label>
     <div class="col-sm-10">
-      <input class="form-control" id="supervisoradd" disabled>
+      <input class="form-control" id="supervisoradd" value="{{ $alumni->supervisoradd }}" disabled disabled>
     </div>
   </div>
 
